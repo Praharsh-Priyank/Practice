a69d1c3e7608d78d3e4850c8b6459c1470bf212c
Praharsh-Priyank <52607687+Praharsh-Priyank@users.noreply.github.com>
2020-11-30 23:32:17 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "/src/components/Heading.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading></Heading>
+    </div>
+  );
+}
+
+export default App;
